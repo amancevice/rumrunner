f4a668c323923acc47eedfbcb8adfced16198663
Alexander Mancevice <amancevice@cargometrics.com>
2019-07-15 11:30:23 -0400

    manifest fixes

--- lib/cargofile/manifest.rb
+++ lib/cargofile/manifest.rb
@@ -5,123 +5,143 @@ module Cargofile
 
     def_delegator :@env, :<<, :env
     def_delegator :@root, :to_s, :root
-    def_delegators :@image, :registry, :username, :name, :tag, :to_s
+    def_delegators :@image, :registry, :username, :name, :tag
 
     def initialize(name:, root:nil, &block)
-      @root      = root || :".docker"
-      @image     = Docker::Image.parse(name)
-      @env       = []
-      @stages    = {}
-      @artifacts = {}
-      @shells    = {}
+      @name  = name
+      @root  = root || :".docker"
+      @image = Docker::Image.parse(name)
+      @env   = []
       instance_eval(&block) if block_given?
     end
 
-    def stage(name_deps, &block)
-      name, deps = name_deps.is_a?(Hash) ? name_deps.first : [name_deps, nil]
+    def build(*args, &block)
+      name, _, deps = Rake.application.resolve_args(args)
+      task name => deps do
+        sh Docker::Build.new(&block).to_s
+      end
+    end
 
+    def run(*args, &block)
+      name, _, deps = Rake.application.resolve_args(args)
+      task name => deps do
+        sh Docker::Run.new(&block).to_s
+      end
+    end
+
+    def stage(*args, &block)
+      name, _, deps = Rake.application.resolve_args(args)
+
+      # Assemble image/iidfile from manifest/stage name
       image   = "#{@image}-#{name}"
       iidfile = File.join(root, image)
-      prereqs = [deps].flatten.compact
-      options = build_options.iidfile(iidfile).tag(image).target(name)
-      command = Docker::Build.new(options: options.to_h, &block)
+      iidpath = File.split(iidfile).first
 
-      @shells[name] = Docker::Run.new.interactive(true).rm(true).tty(true).cmd("/bin/bash")
-      @stages[name] = {iidfile: iidfile, prereqs: prereqs, command: command}
+      # Ensure path to iidfile exists
+      iiddeps = if deps.empty?
+        directory iidpath
+        iidpath
+      else
+        deps.map{|x| File.join(root, "#{@image}-#{x}") }
+      end
+
+      # Build stage and save digest in iidfile
+      file iidfile => iiddeps do
+        build = Docker::Build.new(options: build_options, &block)
+        build.with_defaults(iidfile: iidfile, tag: image, target: name)
+        sh build.to_s
+      end
+
+      # Shortcut to build stage by name
+      desc "Build `#{name}` stage"
+      task name => iidfile
+
+      # Shell into stage
+      desc "Shell into `#{name}` stage"
+      task :"#{name}:shell" => iidfile do
+        digest = File.read(iidfile)
+        run = Docker::Run.new(options: run_options)
+          .interactive(true)
+          .rm(true)
+          .tty(true)
+          .image(digest)
+          .cmd("/bin/bash")
+        sh run.to_s
+      end
+
+      # Clean stage
+      desc "Remove any temporary images and products from `#{name}` stage"
+      task :"#{name}:clean" do
+        if File.exists? iidfile
+          sh "docker", "image", "rm", "--force", File.read(iidfile)
+          rm iidfile
+        end
+      end
+
+      # Add stage to general clean
+      task :clean => :"#{name}:clean"
+
+      # Ensure subsequent stages are cleaned before this one
+      deps.each{|dep| task :"#{dep}:clean" => :"#{name}:clean" }
     end
 
-    def artifact(name_deps, &block)
-      name, deps = name_deps.is_a?(Hash) ? name_deps.first : [name_deps, nil]
+    def artifact(*args, &block)
+      name, _, deps = Rake.application.resolve_args(args)
 
-      options = run_options.rm(true)
-      prereqs = [deps].flatten.compact
-      command = Docker::Run.new(options: options, &block)
+      target  = deps.first
+      image   = "#{@image}-#{target}"
+      iidfile = File.join(root, image)
+      path    = File.split(name).first
+      deps    = [iidfile]
 
-      @artifacts[name] = {prereqs: prereqs, command: command}
+      unless path == "."
+        directory path
+        deps << path
+      end
+
+      desc "Build `#{name}`"
+      file name => deps do
+        digest = File.read(iidfile)
+        run = Docker::Run.new(options: run_options, image: digest, cmd: ["cat", name], &block)
+        run.with_defaults(rm: true)
+        sh "#{run} > #{name}"
+      end
+
+      task :clean do
+        rm name if File.exists?(name)
+        rm_r path if Dir.exists?(path) && path != "."
+      end
     end
 
-    def shell(name_deps, &block)
-      name, deps = name_deps.is_a?(Hash) ? name_deps.first : [nil, name_deps]
+    def shell(*args, &block)
+      target  = Rake.application.resolve_args(args).first
+      name    = :"#{target}:shell"
+      image   = "#{@image}-#{target}"
+      iidfile = File.join(root, image)
 
-      options = run_options.interactive(true).rm(true).tty(true)
-      command = Docker::Run.new(options: options.to_h, &block)
-      command.cmd name unless name.nil?
+      Rake::Task[name].clear if Rake::Task.task_defined?(name)
 
-      @shells[deps] = command
+      desc "Shell into `#{name}` stage"
+      task name => iidfile do
+        digest = File.read(iidfile)
+        run = Docker::Run.new(options: run_options, image: digest, &block)
+        run.with_defaults(interactive:true, rm: true, tty: true)
+        sh run.to_s
+      end
     end
 
     def install
-      directory root
-
-      @stages   .each{|name, target| install_stage    name, target }
-      @artifacts.each{|name, target| install_artifact name, target }
-      @shells   .each{|name, target| install_shell    name, target }
-
       install_clean
     end
 
     private
 
     def build_options
-      Docker::OptionCollection.new build_arg: @env
+      Docker::Options.new(build_arg: @env)
     end
 
     def run_options
-      Docker::OptionCollection.new env: @env
-    end
-
-    def install_stage(name, target)
-      command = target[:command]
-      iidfile = target[:iidfile]
-      iidpath = File.split(iidfile).first
-      prereqs = target[:prereqs].map{|x| @stages[x][:iidfile] }.flatten
-      prereqs << iidpath
-
-      directory iidpath
-
-      file iidfile => prereqs do
-        sh command.to_s
-      end
-
-      desc "Build `#{name}` stage"
-      task name => iidfile
-
-      preclean = @stages.select{|k,v| v[:prereqs].include? name }.keys.map{|x| :"#{x}:clean" }
-      desc "Remove any temporary images and products from `#{name}` stage"
-      task :"#{name}:clean" => preclean do
-        if File.exists? iidfile
-          sh "docker", "image", "rm", "--force", File.read(iidfile)
-          rm iidfile
-        end
-      end
-    end
-
-    def install_artifact(name, target)
-      path    = File.split(name).first
-      command = target[:command]
-      iidfile = target[:prereqs].map{|x| @stages[x][:iidfile] }.flatten.first
-
-      directory path
-
-      desc "Build `#{name}`"
-      file name => [iidfile, path] do
-        command.image File.read(iidfile)
-        command.cmd "cat", name if command.cmd.nil?
-        sh "#{command} > #{name}"
-      end
-      task :clean do
-        rm_r name if File.exists?(name)
-      end
-    end
-
-    def install_shell(name, command)
-      iidfile = @stages[name][:iidfile]
-
-      desc "Shell into `#{name}` stage"
-      task :"#{name}:shell" => iidfile do
-        command.image File.read(iidfile)
-        sh command.to_s
-      end
+      Docker::Options.new(env: @env)
     end
 
     def install_clean
