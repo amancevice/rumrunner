5960be54c4cb2ff584b94be1cedf262d26aa5255
Alexander Mancevice <amancevice@cargometrics.com>
2019-07-28 16:34:13 -0400

    remove codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
 
 require "bundler/setup"
 require "pry"
