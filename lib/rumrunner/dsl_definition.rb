5f4e10497702ae7ccc377cdb2419f1614e36ddba
Alexander Mancevice <amancevice@cargometrics.com>
2020-01-14 13:49:15 -0500

    expand Rum::DSL

--- lib/rumrunner/dsl_definition.rb
+++ lib/rumrunner/dsl_definition.rb
@@ -2,6 +2,7 @@
 require "rake"
 
 require "rumrunner/manifest"
+require "rumrunner/tasks"
 
 module Rum
 
@@ -21,7 +22,35 @@ module Rum
     #   end
     #
     def rum(image, **options, &block)
-      Manifest.install_tasks(image, **options, &block)
+      Rum.application.in_manifest(image, **options, &block)
+    end
+
+    def artifact(*args, &block)
+      ArtifactTask.define_task(*args).install(&block)
+    end
+
+    def build(*args, &block)
+      BuildTask.define_task(*args).install(&block)
+    end
+
+    def env(*args)
+      args.map{|env| Rum.application.current_manifest.env << env }
+    end
+
+    def export(*args, &block)
+      ExportTask.define_task(*args).install(&block)
+    end
+
+    def run(*args, &block)
+      RunTask.define_task(*args).install(&block)
+    end
+
+    def shell(*args, &block)
+      ShellTask.define_task(*args).install(&block)
+    end
+
+    def stage(*args, &block)
+      StageTask.define_task(*args).install(&block)
     end
   end
 end
